0f178777febabd70d854010c7f56ab668e05e7f4
PavelSnizhko <pavel.snizhko.2000@gmail.com>
2021-03-03 07:19:48 +0200

    separet file into logic folders

---
+++ BachelorProject/Helpers/Router.swift
@@ -0,0 +1,20 @@
+//
+//  Router.swift
+//  BachelorProject
+//
+//  Created by Павел Снижко on 03.03.2021.
+//
+
+import UIKit
+import Foundation
+
+class Router {
+    static let shared = Router()
+    
+    private init () {}
+        
+    func buildRoot() {
+        //TODO: implement this func 
+    }
+    
+}
